d9b09247d36007efa655bc3e5c6728970eb1cfc9
Angel M. <kenielpro12345xd@gmail.com>
2026-05-19 14:32:51 -0500

    Add files via upload

---
+++ PC2/src/pc2/ConexionBD.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pc2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ConexionBD {
+    //CONEXION MYSQL
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String CONN_STRING = "jdbc:mysql://localhost:3306/madai";
+
+    private static Connection conn = null;
+
+    private ConexionBD() {
+        // Constructor privado para evitar que se cree una instancia de esta clase
+    }
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                // Carga el controlador de MySQL
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Crea la conexión a la base de datos
+                conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            } catch (ClassNotFoundException | SQLException e) {
+                // Manejo de errores
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+    
+}
